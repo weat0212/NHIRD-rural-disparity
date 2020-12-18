38c54dafbd02550cbbd222ea61ad3ede2eb82175
weat0212 <32704414+weat0212@users.noreply.github.com>
2020-12-18 19:43:57 +0800

    Add raural data

--- cityEncode.R
+++ cityEncode.R
@@ -3,7 +3,7 @@
 # **** CITY Encode ****
 # *********************
 
-# µû¶qµ¥¯Å
+# City Encoding Code
 
 library(dplyr)
 library(stringr)
@@ -13,3 +13,8 @@ city_encode <- read.csv(file.choose(), header = FALSE, encoding = "UTF-8")
 # substring
 city_encode <- str_split_fixed(city_encode$V1, " ", 4)
 city_encode <- city_encode[,c(1:2)]
+
+# Add rural dataset
+rural <- read.csv(file.choose(), header = FALSE, encoding = "BIG5")
+
+
